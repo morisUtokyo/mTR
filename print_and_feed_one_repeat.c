0a6ab8448ab718dc2d9e4d7031de72277ed626af
Shinichi Morishita <moris@edu.k.u-tokyo.ac.jp>
2019-04-02 17:55:55 +0900

    Add files via upload

--- print_and_feed_one_repeat.c
+++ print_and_feed_one_repeat.c
@@ -13,24 +13,24 @@
 
 void print_one_repeat_in_read(repeat_in_read rr){
     printf(
-            "%.50s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\t%d\t%d\t%d\t%d\t%d\t%s\n",
-            //rr.ID,
-            rr.readID,
-            rr.inputLen,
-            rr.rep_start,
-            rr.rep_end,
-            rr.repeat_len,
-            rr.rep_period,
-            rr.Num_freq_unit,
-            rr.Num_matches,
-            (float)rr.Num_matches/rr.repeat_len,
-            rr.Num_mismatches,
-            rr.Num_insertions,
-            rr.Num_deletions,
-            rr.Kmer,
-            rr.ConsensusMethod,
-            rr.string
-            );
+           "%.50s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\t%d\t%d\t%d\t%d\t%d\t%s\n",
+           //rr.ID,
+           rr.readID,
+           rr.inputLen,
+           rr.rep_start,
+           rr.rep_end,
+           rr.repeat_len,
+           rr.rep_period,
+           rr.Num_freq_unit,
+           rr.Num_matches,
+           (float)rr.Num_matches/rr.repeat_len,
+           rr.Num_mismatches,
+           rr.Num_insertions,
+           rr.Num_deletions,
+           rr.Kmer,
+           rr.ConsensusMethod,
+           rr.string
+           );
     fflush(stdout);
 }
 
