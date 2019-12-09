34ef505cff356a49f2f0a61fdcf4ced6966c946c
Shinichi Morishita <moris@edu.k.u-tokyo.ac.jp>
2019-12-09 16:42:13 +0900

    Add files via upload

--- test_single_TR/test.sh
+++ test_single_TR/test.sh
@@ -4,16 +4,17 @@
 mTR=../mTR
 rand_seq=util/rand_seq
 count_match=util/count_match
+comp_mTR_DP=util/comp_mTR_DP
 # temporary directory
 tmp=tmp
 # error pattern
-substitution_rate=10
+substitution_rate=5 #13
 insertion_rate=3
-deletion_rate=8
+deletion_rate=9 #5
 # number of sequences
-num_rand_seqs=1000
+num_rand_seqs=1000 #100
 # Length of random sequences before and after the tandem repeat
-rand_seq_len=1000
+rand_seq_len=1000 #1000
 
 
 echo "This program generates a fasta file with "$num_rand_seqs" sequences with tandem repeats according to the following error pattern, predicts the unit string for each sequence, and outputs the number of correct answers in the prediction."
@@ -23,20 +24,30 @@ echo "insertion rate    = " $insertion_rate "(%)"
 echo "deletion rate     = " $deletion_rate "(%)"
 
 # j frequency of units
-for j in 10 30 50 100 200; do
+#for j in 10 30 50 100 200; do
+for j in 10 30 50; do
 # i unit length
-for i in 2 3 4 5 6 7 8 9 10 50 100 200; do
+#for i in 2 5 10; do
+for i in 2 5 10 50 100 200; do
 echo "------------------"
 echo "Frequency of units = "$j", Length of units = "$i
 $rand_seq $tmp"/rand_seq.fasta" $tmp"/rand_unit.txt" $i $j $substitution_rate $insertion_rate $deletion_rate $rand_seq_len $rand_seq_len $num_rand_seqs
-$mTR -s $tmp"/rand_seq.fasta" > $tmp"/rand_TRs.txt"
+$mTR $tmp"/rand_seq.fasta" > $tmp"/rand_TRs.txt"
+
 echo "Number of correct answers for" $num_rand_seqs "sequences with tandem repeats"
 $count_match $tmp"/rand_TRs.txt" $tmp"/rand_unit.txt"
+$comp_mTR_DP $tmp"/rand_unit.txt" $tmp"/rand_TRs.txt" $tmp"/comp_mTR_DP_result.txt"
+echo "Number of answers with match ratios >= 1"
+cat $tmp"/comp_mTR_DP_result.txt"  | awk '$1>=1' | wc -l
+echo "Number of answers with match ratios >= 0.99"
+cat $tmp"/comp_mTR_DP_result.txt"  | awk '$1>=0.99' | wc -l
+echo "Number of answers with match ratios >= 0.98"
+cat $tmp"/comp_mTR_DP_result.txt"  | awk '$1>=0.98' | wc -l
 done
 done
 
-rm $tmp"/rand_seq.fasta"
-rm $tmp"/rand_unit.txt"
-rm $tmp"/rand_TRs.txt"
+#rm $tmp"/rand_seq.fasta"
+#rm $tmp"/rand_unit.txt"
+#rm $tmp"/rand_TRs.txt"
 
 exit 0
