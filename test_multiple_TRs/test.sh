f1f9b77dd51debe0c9bedb3972a29be666aa63f6
Shinichi Morishita <moris@edu.k.u-tokyo.ac.jp>
2020-04-12 21:58:41 +0900

    Add files via upload

--- test_multiple_TRs/test.sh
+++ test_multiple_TRs/test.sh
@@ -2,15 +2,6 @@
 
 mTR=../mTR
 
-# Nanopore reads with long tandem repeats from C. elegans strain PD1074
-echo "-----------------------------------"
-#./mTR -s ../multiTR/data/worm_chrI.fasta
-$mTR -m data/worm_chrI.fasta
-echo "-----------------------------------"
-$mTR -m data/worm_chrII_1.fasta
-echo "-----------------------------------"
-$mTR -m data/worm_chrII_2.fasta
-
 # Synthetic reads with two types of tandem repeats.
 # The lengths of two tandem repeats are i and j.
 # File named i_j_unit.txt contain the unit strings of length i and j.
@@ -18,15 +9,25 @@ for i in 3 5 10 20 50; do
 for j in 3 5 10 20 50; do
 if [ $i -lt $j ]; then
 echo $i"_"$j"-----------------------------------"
-$mTR -m "data/"$i"_"$j".fasta"
+$mTR "data/"$i"_"$j".fasta"
 fi
 done
 done
 
 # Reads with four types of tandem repeats
-$mTR -m data/2_5_10_20_set.fasta
+echo "2_5_10_20 -----------------------------------"
+$mTR data/2_5_10_20_set.fasta
 
 # Reads with seven types of tandem repeats
-$mTR -m data/2_5_10_20_50_100_200_set.fasta
+echo "2_5_10_20_50_100_200 -----------------------------------"
+$mTR data/2_5_10_20_50_100_200_set.fasta
+
+# Nanopore reads with long tandem repeats from C. elegans strain PD1074
+echo "-----------------------------------"
+$mTR data/worm_chrI.fasta
+echo "-----------------------------------"
+$mTR data/worm_chrII_1.fasta
+echo "-----------------------------------"
+$mTR data/worm_chrII_2.fasta
 
 exit 0
